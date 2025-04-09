cfa9eebb638bed856ee4fba2a211d677bce8a294
Damir1974 <damir.24.usa@gmail.com>
2025-04-09 16:46:08 -0400

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -1,7 +1,1 @@
-import telegram
-
-TOKEN = '8067243807:AAH3xot3O0iEx_c1BSWPwAMrqf-0OZ-lB1w'
-CHAT_ID = '956286581'
-
-bot = telegram.Bot(token=TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="Проверка связи. Катя здесь.")
+)
